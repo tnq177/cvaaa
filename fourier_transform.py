6aef044696dd451f5ec516fb2189598c62cbe466
Toan Nguyen <toannq177@gmail.com>
2015-10-24 17:28:33 +0700

    [f] FFT of sinusoidal signals along diagonal

--- fourier_transform.py
+++ fourier_transform.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy
 import cv2
 
@@ -20,21 +20,36 @@ def get_plottable_fft(x):
 # cv2.imshow('centered fft', get_plottable_fft(bbc_fft_centered))
 # cv2.waitKey()
 
-# Fourier transform of Cosine if average of two impulses at f & -f, with f = frequency of cosine function
-cosine_x = numpy.zeros((500, 500), numpy.float)
-cosine_y = numpy.zeros((500, 500), numpy.float)
+# # Fourier transform of Cosine if average of two impulses at f & -f, with f = frequency of cosine function
+# cosine_x = numpy.zeros((500, 500), numpy.float)
+# cosine_y = numpy.zeros((500, 500), numpy.float)
 
-for i in xrange(500):
-    cosine_x[:, i].fill(255 * numpy.cos(100 * i))
-    cosine_y[i].fill(255 * numpy.cos(100 * i))
+# for i in xrange(500):
+#     cosine_x[:, i].fill(255 * numpy.cos(100 * i))
+#     cosine_y[i].fill(255 * numpy.cos(100 * i))
 
-cosine_x_fft_centered = numpy.fft.fftshift(numpy.fft.fft2(cosine_x))
-cosine_y_fft_centered = numpy.fft.fftshift(numpy.fft.fft2(cosine_y))
+# cosine_x_fft_centered = numpy.fft.fftshift(numpy.fft.fft2(cosine_x))
+# cosine_y_fft_centered = numpy.fft.fftshift(numpy.fft.fft2(cosine_y))
 
-cosine_x[cosine_x < 0] = 0
-cosine_y[cosine_y < 0] = 0
-cv2.imshow('cosine x', cosine_x.astype(numpy.uint8))
-cv2.imshow('cosine y', cosine_y.astype(numpy.uint8))
-cv2.imshow('cosine x fft', get_plottable_fft(cosine_x_fft_centered))
-cv2.imshow('cosine y fft', get_plottable_fft(cosine_y_fft_centered))
+# cosine_x[cosine_x < 0] = 0
+# cosine_y[cosine_y < 0] = 0
+# cv2.imshow('cosine x', cosine_x.astype(numpy.uint8))
+# cv2.imshow('cosine y', cosine_y.astype(numpy.uint8))
+# cv2.imshow('cosine x fft', get_plottable_fft(cosine_x_fft_centered))
+# cv2.imshow('cosine y fft', get_plottable_fft(cosine_y_fft_centered))
+# cv2.waitKey()
+
+# If fft is an impulse, it should result in a sinusoidal along x & y
+img = numpy.zeros((500, 500), numpy.float)
+
+# Very stupid
+for x in xrange(500):
+    for y in xrange(500):
+        angle = numpy.pi * 3 / 4 - numpy.arctan2(y, x)
+        r = numpy.sqrt(x ** 2 + y ** 2) * numpy.sin(angle) / (numpy.sin(numpy.pi / 4) * numpy.sqrt(2))
+        img[x, y] = numpy.cos(100 * r)
+
+fft_shift = numpy.fft.fftshift(numpy.fft.fft2(img))
+cv2.imshow('img', img)
+cv2.imshow('fft', get_plottable_fft(fft_shift))
 cv2.waitKey()
