5a93648034717e45316d151533d4685807b5e138
Toan Nguyen <toannq177@gmail.com>
2015-10-25 20:18:01 +0700

    [f] Add unsharp masking

---
+++ unsharp_masking.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import numpy
+import cv2
+
+def trackbar_changed(x):
+    pass 
+
+img = cv2.imread('./data/eye.png')
+blurred = cv2.GaussianBlur(img, (3, 3), sigmaX=0, sigmaY=0)
+gamma = 0
+
+cv2.namedWindow('img')
+cv2.namedWindow('blurred')
+cv2.namedWindow('unsharp masking')
+cv2.createTrackbar('gamma', 'unsharp masking', 1, 500, trackbar_changed)
+
+while True:
+    gamma = cv2.getTrackbarPos('gamma', 'unsharp masking') / 100
+    unsharp_masking = cv2.addWeighted(img, 1 + gamma, blurred, -gamma, 0)
+    cv2.imshow('img', img)
+    cv2.imshow('blurred', blurred)
+    cv2.imshow('unsharp masking', unsharp_masking)
+    
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    
+
+
