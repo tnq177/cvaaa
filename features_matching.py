d2315656a6f70f4de00774a859c5fc1130c3b4da
Toan Nguyen <toannq177@gmail.com>
2015-10-28 12:30:43 +0700

    [f] Harris Corner & Features matching

---
+++ features_matching.py
@@ -0,0 +1,31 @@
+# Not my code, taken from OpenCV Tutorial
+import numpy
+import cv2
+
+img1 = cv2.imread('./data/iphone_1.JPG', 0)
+img2 = cv2.imread('./data/iphone_2.JPG', 0)
+
+# ORB detector
+orb = cv2.ORB_create()
+
+# Key points & descriptor
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+# BFMatcher with Hamming distance
+# Return the nearest neighbor only
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Matchers
+matches = bf.match(des1, des2)
+
+# Match the descriptors
+matches = sorted(matches, key=lambda x: x.distance)
+
+# Draw first 10 matches
+for i in xrange(10):
+    img3 = numpy.zeros(img1.shape)
+    img3 = cv2.drawMatches(
+        img1, kp1, img2, kp2, matches[i: i+1], img3, flags=2)
+    cv2.imshow('img', img3)
+    cv2.waitKey()
---
+++ harris_corner.py
@@ -0,0 +1,48 @@
+from __future__ import print_function, division
+import numpy
+import cv2
+from scipy.ndimage import maximum_filter
+
+file_name = 'data/building.png'
+block_size = 2
+aperture_size = 3
+k = 0.04
+
+img = cv2.imread(file_name)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = numpy.float32(gray)
+
+# OpenCV
+dst = cv2.cornerHarris(gray, block_size, aperture_size, k)
+
+# Dilate the result to make the corner bigger for display
+dst = cv2.dilate(dst, None)
+opencv_img = img.copy()
+opencv_img[dst > dst.max() * 0.01] = [0, 0, 255]
+
+# Mine
+Ix = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=aperture_size)
+Iy = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=aperture_size)
+
+A = Ix * Ix
+B = Iy * Iy
+C = Ix * Iy
+A = cv2.GaussianBlur(A, ksize=(0, 0), sigmaX=1, sigmaY=1)
+B = cv2.GaussianBlur(B, ksize=(0, 0), sigmaX=1, sigmaY=1)
+C = cv2.GaussianBlur(C, ksize=(0, 0), sigmaX=1, sigmaY=1)
+
+R = (A * B - (C ** 2)) - k * ((A + B) ** 2)
+
+# Surpress non local maxima
+threshold = R.max() * 0.01
+local_maxima = numpy.where(
+    maximum_filter(R, size=(block_size, block_size)) == R, R, 0)
+
+local_maxima = cv2.dilate(local_maxima, None)
+mine_img = img.copy()
+mine_img[local_maxima > threshold] = [0, 0, 255]
+
+
+cv2.imshow('Opencv harris corner', opencv_img)
+cv2.imshow('My harris corner', mine_img)
+cv2.waitKey()
